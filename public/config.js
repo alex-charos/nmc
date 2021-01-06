d56fd6048e60d5ba5f911c71364640356148e856
Alex <realtowz@yahoo.com>
2021-01-06 13:24:04 +0200

    mailing enabled, popup added, minor refactoring started

---
+++ public/config.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCv4CNcRDf_ef92vnKmsMCasIeiAKI0H60",
+  authDomain: "nm-configurator.firebaseapp.com",
+  databaseURL: "https://nm-configurator-default-rtdb.firebaseio.com",
+  projectId: "nm-configurator",
+  storageBucket: "nm-configurator.appspot.com",
+  messagingSenderId: "739965864157",
+  appId: "1:739965864157:web:030b1a239b7414fbb57334",
+  measurementId: "G-N43N8EK0NF"
+};
+
+
+var config ={firebase:firebaseConfig,
+	 mailUrl:'https://europe-west1-nm-configurator.cloudfunctions.net/nmc-mail-fn'
+	};
